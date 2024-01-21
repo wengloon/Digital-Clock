36aede3e45c9fb7d83474edf8197baac71fb4016
Weng Loon <wengloon0903@hotmail.com>
2024-01-21 18:19:21 +0800

    【 Display update 】 Clock Hour display support

--- Digital_Clock.v
+++ Digital_Clock.v
@@ -3,24 +3,30 @@ module Digital_Clock(
 	output [6:0] SecLoSeg,
 	output [6:0] SecHiSeg, 
 	output [6:0] MinLoSeg, 
-	output [6:0] MinHiSeg 
+	output [6:0] MinHiSeg, 
+	output [6:0] HrLoSeg, 
+	output [6:0] HrHiSeg
 	);
 	//time display
 	//h2 h1 : m2 m1 : s2 s1
 	
 	
-	reg [3:0] InSecLo, InSecHi, InMinLo, InMinHi;
+	reg [3:0] InSecLo, InSecHi, InMinLo, InMinHi, InHrLo, InHrHi;
 	reg [5:0] Sec, Min;
+	reg [4:0] Hour;
 	reg [31:0]segclk;
 	
 	initial
 		begin
 			Sec <= 6'd0;
 			Min <= 6'd0;
+			Hour <= 5'd0;
 			InSecLo <= 4'd0;
 			InSecHi <= 4'd0;
 			InMinLo <= 4'd0;
 			InMinHi <= 4'd0;
+			InHrLo <= 4'd0;
+			InHrHi <= 4'd0;
 		end
 	
 	always @( posedge clk )
@@ -35,9 +41,14 @@ module Digital_Clock(
 							Sec <= 6'd0;
 							Min = Min + 1'b1;
 							if ( Min >= 6'd60 )
-							begin
-								Min <= 6'd0;
-							end
+								begin
+									Min <= 6'd0;
+									Hour = Hour + 1'b1;
+									if ( Hour >= 5'd24 )
+										begin
+											Hour <= 6'd0;
+										end
+								end
 						end
 				end
 			end
@@ -48,11 +59,15 @@ module Digital_Clock(
 			InSecLo=Sec%10;		//5/10 = ""0""
 			InMinHi=Min/10;
 			InMinLo=Min%10;
+			InHrHi=Hour/10;
+			InHrLo=Hour%10;
 		end
 
 		Decoder7Segment secLo( .In(InSecLo), .segmentDisplay(SecLoSeg));
 		Decoder7Segment secHi( .In(InSecHi), .segmentDisplay(SecHiSeg));
 		Decoder7Segment minLo( .In(InMinLo), .segmentDisplay(MinLoSeg));
 		Decoder7Segment minHi( .In(InMinHi), .segmentDisplay(MinHiSeg));
+		Decoder7Segment hrLo( .In(InHrLo), .segmentDisplay(HrLoSeg));
+		Decoder7Segment hrHi( .In(InHrHi), .segmentDisplay(HrHiSeg));
 
 endmodule
