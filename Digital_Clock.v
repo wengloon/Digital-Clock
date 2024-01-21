dca1c28e4ebbca6a8ef9f12421285be4d7b06462
Weng Loon <wengloon0903@hotmail.com>
2024-01-21 15:36:16 +0800

    【 Display update 】 Clock minute display support
    Minute display available

--- Digital_Clock.v
+++ Digital_Clock.v
@@ -1,19 +1,23 @@
 module Digital_Clock(
 	input clk,
 	output [6:0] SecLoSeg,
-	output [6:0] SecHiSeg
+	output [6:0] SecHiSeg, 
+	output [6:0] MinLoSeg, 
+	output [6:0] MinHiSeg 
 	);
 	//time display
 	//h2 h1 : m2 m1 : s2 s1
 	
 	
-	reg [3:0] InSecLo, InSecHi;
+	reg [3:0] InSecLo, InSecHi, InMinLo, InMinHi;
 	reg [31:0]segclk;
 	
 	initial
 		begin
 			InSecLo <= 4'd0;
 			InSecHi <= 4'd0;
+			InMinLo <= 4'd0;
+			InMinHi <= 4'd0;
 		end
 	
 	always @( posedge clk )
@@ -30,6 +34,16 @@ module Digital_Clock(
 							if ( InSecHi >= 4'd6 )
 								begin
 									InSecHi <= 4'd0;
+									InMinLo = InMinLo + 1'b1;
+									if ( InMinLo >= 4'd10 )
+										begin
+											InMinLo <= 4'd0;
+											InMinHi = InMinHi + 1'b1;
+											if ( InMinHi >= 4'd6 )
+												begin
+													InMinHi<= 4'd0;
+												end
+										end
 								end
 						end
 				end
@@ -37,5 +51,7 @@ module Digital_Clock(
 
 		Decoder7Segment secLo( .In(InSecLo), .segmentDisplay(SecLoSeg));
 		Decoder7Segment secHi( .In(InSecHi), .segmentDisplay(SecHiSeg));
+		Decoder7Segment minLo( .In(InMinLo), .segmentDisplay(MinLoSeg));
+		Decoder7Segment minHi( .In(InMinHi), .segmentDisplay(MinHiSeg));
 
 endmodule
