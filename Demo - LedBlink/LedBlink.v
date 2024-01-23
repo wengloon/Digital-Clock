ceab15952a265c13ac8a3a22b47dc48abf1e82ac
Weng Loon <wengloon0903@hotmail.com>
2024-01-23 20:58:33 +0800

    【 File Maintain 】 Project categorize

---
+++ Demo - LedBlink/LedBlink.v
@@ -0,0 +1,20 @@
+module LedBlink (
+	input clk,
+	output[9:0] LED
+);
+
+reg [22:0] cnt;	
+
+initial 
+begin
+cnt <= 32'h00000000;
+end
+
+always @ (posedge clk ) begin
+cnt<= cnt + 1;
+end
+
+assign LED[0] = cnt[22];
+assign LED[9:1] = 9'b000000000;
+
+endmodule 
