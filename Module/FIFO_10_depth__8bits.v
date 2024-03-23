e619db2d72a608323d8662de8e0778892d7a7b55
Weng Loon <wengloon0903@hotmail.com>
2024-03-24 01:15:40 +0800

    【 Module 】  remove unnecessary indicator

--- Module/FIFO_10_depth__8bits.v
+++ Module/FIFO_10_depth__8bits.v
@@ -4,9 +4,9 @@ input [7:0]i_din,
 input i_wr,
 input i_rd,
 input i_rst_n,
-output reg [9:0]o_led,
 output o_empty,
 output o_full,
+output reg o_data_ready,
 output reg [7:0]o_dout
 );
 
@@ -19,15 +19,7 @@ wire [7:0] read_data;
 
 assign o_empty = ( write_idx == read_idx ) ? 1'd1 : 1'd0;	//read index same as write index
 assign o_full = ( write_idx + 4'd1 == read_idx ) ? 1'd1 : 1'd0; // read index was infront of write index by 1 only
-always @ ( posedge i_clk or negedge i_rst_n)
-	begin
-		if( ~i_rst_n )
-			begin
-				o_led <= 0;
-			end
-		o_led[write_idx] <= 1;
-	end
-	
+
 
 always @ ( posedge i_clk or negedge i_rst_n)
 	begin
@@ -62,11 +54,16 @@ always @ ( posedge i_clk or negedge i_rst_n)
 							begin
 								o_dout <= buffer[read_idx];
 								read_idx <= read_idx + 4'd1;
+								o_data_ready <= 1'd1;
 							end
 						else
 							begin
 								o_dout <= 7'd0;
 							end
+					end
+				else
+					begin
+						o_data_ready <= 1'd0;
 					end
 			end
 	end
